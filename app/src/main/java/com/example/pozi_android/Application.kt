9c3465acf22763ecc6cca74e29ebe4853d8a2ffd
Min Gyu <tnvnfdla12@gmail.com>
2022-07-17 20:34:42 +0900

    chore : first-commit

---
+++ app/src/main/java/com/example/pozi_android/Application.kt
@@ -0,0 +1,23 @@
+package com.example.pozi_android
+
+import android.app.Application
+import android.content.Context
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Application : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        appContext = this
+    }
+
+    override fun onTerminate() {
+        super.onTerminate()
+        appContext = null
+    }
+
+    companion object {
+        var appContext: Context? = null
+            private set
+    }
+}
