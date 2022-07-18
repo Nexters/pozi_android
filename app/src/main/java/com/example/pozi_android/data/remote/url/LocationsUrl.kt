416572379139b3c027182145d1d348b326273467
Min Gyu <tnvnfdla12@gmail.com>
2022-07-18 14:07:12 +0900

    mock 데이터 수정 후 마커 찍히는지 확인했습니다

--- app/src/main/java/com/example/pozi_android/data/remote/url/LocationsUrl.kt
+++ app/src/main/java/com/example/pozi_android/data/remote/url/LocationsUrl.kt
@@ -2,5 +2,5 @@ package com.example.pozi_android.data.remote.url
 
 object LocationsUrl {
     const val BASE_URL = "https://run.mocky.io"
-    const val LOCATION_URL = "/v3/0ec238c7-aea5-4b6e-b832-426acf16b3d8"
+    const val LOCATION_URL = "/v3/9e6003ea-0d5f-4e15-9b4e-c394e49c73c1"
 }
