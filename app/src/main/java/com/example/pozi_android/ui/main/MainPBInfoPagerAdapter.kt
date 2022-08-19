da00abee7e770e624359c7aac3f468c10110b65b
sangeun <suev7245@gmail.com>
2022-08-20 01:09:52 +0900

    [45+QA] 이미지 2배수이미지로 변경 및 glide로 변경

--- app/src/main/java/com/example/pozi_android/ui/main/MainPBInfoPagerAdapter.kt
+++ app/src/main/java/com/example/pozi_android/ui/main/MainPBInfoPagerAdapter.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.pozi_android.R
 import com.example.pozi_android.databinding.ItemPbViewpagerBinding
 import com.example.pozi_android.domain.entity.Place
@@ -27,15 +28,20 @@ class MainPBInfoPagerAdapter :
         fun bind(item: ViewPagerItem) {
             binding.viewpageritem = item
             when (item.place.brandName) {
-                "포토매틱" -> binding.brandimage.setImageResource(R.drawable.brand_photomatic)
-                "하루필름" -> binding.brandimage.setImageResource(R.drawable.brand_harufilm)
-                "셀픽스" -> binding.brandimage.setImageResource(R.drawable.brand_selfixselfix)
-                "포토드링크" -> binding.brandimage.setImageResource(R.drawable.brand_photodrink)
-                "포토그레이" -> binding.brandimage.setImageResource(R.drawable.brand_photogray)
-                "포토이즘" -> binding.brandimage.setImageResource(R.drawable.brand_photoism)
-                "비룸" -> binding.brandimage.setImageResource(R.drawable.brand_broom)
+                "포토매틱" -> setImage(R.drawable.brand_photomatic)
+                "하루필름" -> setImage(R.drawable.brand_harufilm)
+                "셀픽스" -> setImage(R.drawable.brand_selfix)
+                "포토드링크" -> setImage(R.drawable.brand_photodrink)
+                "포토그레이" -> setImage(R.drawable.brand_photogray)
+                "포토이즘" -> setImage(R.drawable.brand_photoism)
+                "비룸" -> setImage(R.drawable.brand_broom)
             }
+        }
 
+        private fun setImage(drawable: Int) {
+            Glide.with(binding.brandimage.context)
+                .load(drawable)
+                .into(binding.brandimage)
         }
     }
 
